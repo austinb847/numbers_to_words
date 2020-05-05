e46df395c09d82ae4d7cf71628ef9e18db8f8c5b
austinb847 <austinb847@icloud.com>
2020-05-05 14:35:49 -0700

    loop through hash and find input number and return it as string

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -7,7 +7,50 @@ class Number
     @num
   end
 
+  def name_of_numbers
+    return {
+      1000000 => "million",
+      1000 => "thousand",
+      100 => "hundred",
+      90 => "ninety",
+      80 => "eighty",
+      70 => "seventy",
+      60 => "sixty",
+      50 => "fifty",
+      40 => "forty",
+      30 => "thirty",
+      20 => "twenty",
+      19 => "nineteen",
+      18 => "eighteen",
+      17 => "seventeen",
+      16 => "sixteen",
+      15 => "fifteen",
+      14 => "fourteen",
+      13 => "thirteen",
+      12 => "twelve",
+      11 => "eleven",
+      10 => "ten",
+      9 => "nine",
+      8 => "eight",
+      7 => "seven",
+      6 => "six",
+      5 => "five",
+      4 => "four",
+      3 => "three",
+      2 => "two",
+      1 => "one"
+    }
+  end
+
   def number_to_word
+    num_string = ''
+    number_names = name_of_numbers()
+    number_names.each() do |number, name|
+      if (@num == number)
+        num_string += name
+      end
+    end
+    num_string
   end
   
 end
